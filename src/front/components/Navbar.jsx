9394d3fdb4b0e01567659d2fa8ec045d122b1123
NicolleMendoza30 <83618165+NicolleMendoza30@users.noreply.github.com>
2025-08-25 01:15:31 +0000

    Home

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light bg-light nav">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+
+				<div className="ml-auto nav">
+					<img src=""></img>
+					<p><a class="link-opacity-75-hover home" href="#">Home</a></p>
+					<p><a class="link-opacity-75-hover mi_despensa" href="#">Mi despensa</a></p>
+					<p><a class="link-opacity-75-hover mis_recetas" href="#">Mis recetas</a></p>
+					<button type="button" class="btn btn-primary login">Login</button>
+					
+
+
 				</div>
 			</div>
 		</nav>
