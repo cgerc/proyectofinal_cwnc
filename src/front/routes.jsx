e2dc232f5c91b86b4689f2dd41e74b8fa12558b2
NicolleMendoza30 <83618165+NicolleMendoza30@users.noreply.github.com>
2025-09-01 22:42:53 +0000

    Home-M

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -27,6 +27,7 @@ export const router = createBrowserRouter(
       <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
       <Route path="/demo" element={<Demo />} />
       <Route path="/midespensa" element={<MiDespensa />} />
+      <Route path="/home" element={<Home/>}/>
 
     </Route>
   )
