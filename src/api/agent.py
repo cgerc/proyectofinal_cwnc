e71da8919086012785ab15b8dd519561e2d61a16
Cristobal <crispinochet@outlook.cl>
2025-09-01 10:47:42 -0400

    feat(API): difficult and time added

--- src/api/agent.py
+++ src/api/agent.py
@@ -12,29 +12,36 @@ agent = Agent(
         api_key=os.getenv("OPENAI_API_KEY")
     ),
     description="soy un asistente que genera recetas de cocina",
-    instructions=["Genera una receta de cocina a partir de los ingredientes que te doy. Responde en formato json con los campos: titulo, ingredientes (lista) y preparacion (lista). Si no me das ningun ingrediente, inventate una receta tu solo."],
+    instructions=[
+        "Responde SIEMPRE con un JSON válido.",
+        "Genera una receta de cocina a solo con los ingredientes que te doy y que la preparacion sea detallada",
+        "Usa solo los ingredientes proporcionados por el usuario; no agregues otros. Si falta algo esencial, indícalo en los ingredientes como sugerencia opcional.",
+        "Responde en formato json con los campos: titulo, ingredientes (lista) y preparacion (lista), dificultad (con una puntuacion entre 1-5), y tiempo estimado de la preparacion.",
+        "Devuelve un objeto con la clave 'opciones', que sea una lista de exactamente 3 recetas.",
+        "Escribe en español y NO añadas texto fuera del JSON."
+        ],
     show_tool_calls=True,
     markdown=True, 
 )
 
 def main():
-    print("--- Agent ---")
-    while True:
+    print("--- CookIA ---")
+    while True: # Bucle para interactuar con el agente
 
-        pregunta = input("tú: ").strip()
+        pregunta = input("tú: ").strip() # Entrada del usuario, osea los ingredientes para la receta y elimina los espacios al final
 
-        if pregunta.lower() in ["exit", "quit", "salir"]:
-            print("Saliendo...")
-            break
-        if not pregunta:
+        if pregunta.lower() in ["exit", "quit", "salir"]: # comandos para salir del bucle (sin importar mayusculas)
+            print("Saliendo...") # mensaje de salida
+            break # ruptura del bucle
+        if not pregunta: # Si la petiicion esta vacia se imprime un mensaje
             print("No se proporcionaron ingredientes.")
-            continue
+            continue # se continua con el bucle
         
-        try:
-            print("agente:", end="", flush=True)
-            respuesta = agent.run(pregunta)
-            print(respuesta.content)
-        except Exception as e:
+        try: #intenta ejecturar el siguiente codigo
+            print("agente:", end="", flush=True) # "agente" y fuerza la impresion inmediata
+            respuesta = agent.run(pregunta) # se ejecuta la respuesta del agente 
+            print(respuesta.content) # impresion de la respuesta
+        except Exception as e: #manejo de error e impresion de un mensaje error
             print("Error:", e)
 
 if __name__ == "__main__":
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,10 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from src.api.models import db, User
 from flask_cors import CORS
-from api.extensions import bcrypt
-from api.utils import generate_sitemap, APIException
+from src.api.extensions import bcrypt
+from src.api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 
