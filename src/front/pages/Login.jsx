4808144534e9fce3c09737049cd1f01e89904488
cgerc <gercanepacarmen@gmail.com>
2025-09-06 19:02:29 +0000

    crear cuenta con hover

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -40,6 +40,28 @@ const Login= () => {
             <div className="text-center" style={{maxWidth: "330px", margin: "auto", padding:"15px"}}>
                 <form className="form-sigin" onSubmit={handleUserSubmit}>
                     <h1 className="h3 mb-3 font-weight-normal">Iniciar sesión</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Correo electrónico</label>
+                    <input onChange={handleChange} name='email' type="email" id="inputEmail" className="form-control" placeholder="Email adress" required="" autoFocus="" />
+                    <label htmlFor="inputPassword" className="sr-only">cONTRASEÑA</label>
+                    <input onChange={handleChange} name='password' type="password" id="inputPassword" className="form-control"placeholder="Contraseña" required="" />
+                    <div className='d-flex align-items-center justify-content-center flex-column'>
+                        <a className="btn btn-lg btn-primary btn-block" type="subtmit">Iniciar sesión</a>
+                        <p className="mt-3">
+                            ¿Eres un usuario nuevo?{ ' '}
+                            <span
+                                style={{
+                                    color: '#007bff'
+                                    cursor: 'pointer',
+                                    textDecoration: 'none'
+                                }}
+                                onMouseOver={(e) => e.target.style.textDecoration= 'underline'}
+                                onMouseOut={(e) =>e.target.style.textDecoration= 'none'}
+                                onClick={() => navigate('/register')}
+                                >
+                                    Crear cuenta
+                                </span>
+                        </p>
+                    </div>
                 </form>
             </div>
     
